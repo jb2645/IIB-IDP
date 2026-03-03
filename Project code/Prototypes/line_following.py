ea93459edff5de817012eeb67e4cc77fd6cfbe2c
jb2645 <jb2645@cam.ac.uk>
2026-03-03 09:17:30 +0000

    Update line_following.py

--- Project code/Prototypes/line_following.py
+++ Project code/Prototypes/line_following.py
@@ -118,6 +118,7 @@ class Position:
             pass
     
 
+<<<<<<< Updated upstream
 class Path_LFT:
     def __init__(self, drive, sensors):
         self.drive = drive
@@ -195,20 +196,26 @@ def main():
     grid = [(3,1),(2,0),(3,1),(2,0),(2,5),(7,6),(9,5),(9,5)]
     bays = [1,2,6,7]
     pos = Position(grid)
+=======
+def main():
+    grid = [(3,1),(2,0),(3,1),(2,0),(2,5),(7,6),(9,5),(9,5)]
+    bays = [1,2,6,7]
+    pos = Position(grid)##
+>>>>>>> Stashed changes
 
     
- #   left_motor = Motor(dirPin=4, PWMPin=5)#check values later
- #   right_motor = Motor(dirPin=6, PWMPin=7)
-#
+    left_motor = Motor(dirPin=4, PWMPin=5)#check values later
+    right_motor = Motor(dirPin=6, PWMPin=7)
+
     #sensors = LineSensors(['''pin1,pin2,pin3,pin4'''])
-#    sensors = LineSensors(6, 7, 8, 9)      #check actual order of gpio pins
+    sensors = Optocoupler(6, 7, 8, 9)      #check actual order of gpio pins
     
- #   drive = MainDrive(left_motor, right_motor)
- #   drive = Rover(left_motor, right_motor, LineSensors) # This will need to be updated later when rover class completed
+    drive = MainDrive(left_motor, right_motor)
+    drive = Rover(left_motor, right_motor, sensors) # This will need to be updated later when rover class completed
     
     path = Path_LFT(drive, sensors)
     
- #   follower = LineFollow(drive, sensors)
+    follower = LineFollow(drive, sensors)
     
     while True:
         event = sensors.get_event()
@@ -225,10 +232,10 @@ def main():
         elif pos.state == "TURN":
             left_value, right_value = sensors.read_junction()
                 
-    #            if left_value>right_value:
+                if left_value>right_value:
                     #turn left
-     #               drive.turnleft()
-      #              pos.turn_end(1)
+                    drive.turnleft()
+                    pos.turn_end(1)
                     
                 else:
                     pos.turn_end(0)
